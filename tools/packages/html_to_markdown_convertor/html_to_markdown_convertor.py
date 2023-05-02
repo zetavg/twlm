01c0ecaf556fb723b2cc6772b967a6897e0123f7
zetavg <mail@zeta.vg>
2023-05-03 03:42:24 +0800

    test html-markdown conversion

--- tools/packages/html_to_markdown_convertor/html_to_markdown_convertor.py
+++ tools/packages/html_to_markdown_convertor/html_to_markdown_convertor.py
@@ -1,32 +1,70 @@
-from markdownify import MarkdownConverter, ATX
+from markdownify import (
+    MarkdownConverter, ATX,
+    NavigableString, Comment, Doctype, html_heading_re, chomp, six)
 import pangu
 import re
+import json
 
 CJK = r'\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff'
-CJK_QUOTE = re.compile('([{CJK}])([`"\u05f4])'.format(CJK=CJK))  # no need to escape `
-QUOTE_CJK = re.compile('([`"\u05f4])([{CJK}])'.format(CJK=CJK))  # no need to escape `
-CJK_ASTERISK_EN = re.compile('([{CJK}])([*][*]?)([a-zA-Z0-9])'.format(CJK=CJK))
-EN_ASTERISK_CJK = re.compile('([a-zA-Z0-9,.?!])([*][*]?)([{CJK}])'.format(CJK=CJK))
-CJK_UNDERLINE_EN = re.compile('([{CJK}])(__?)([a-zA-Z0-9])'.format(CJK=CJK))
-EN_UNDERLINE_CJK = re.compile('(__?)([a-zA-Z0-9,.?!])([{CJK}])'.format(CJK=CJK))
+ANS = r'A-Za-z\u0370-\u03ff0-9@\\$%\\^&\\*\\-\\+\\\\=\\|/\u00a1-\u00ff\u2150-\u218f\u2700—\u27bf'
+
+STARTS_WITH_CJK = re.compile('^[{CJK}]'.format(CJK=CJK))
+ENDS_WITH_CJK = re.compile('[{CJK}]$'.format(CJK=CJK))
+STARTS_WITH_ANS = re.compile('^[{ANS}]'.format(ANS=ANS))
+ENDS_WITH_ANS = re.compile('[{ANS}]$'.format(ANS=ANS))
+
+MD_ANS_TAG_START = re.compile(
+    '^(?:\*\*?|__?|~~|`)[{ANS}]'.format(ANS=re.sub(r'[*_~]', '', ANS)))
+MD_ANS_TAG_END = re.compile(
+    '[{ANS},.](?:\*\*?|__?|~~|`)$'.format(ANS=re.sub(r'[*_~]', '', ANS)))
+MD_CJK_TAG_START = re.compile('^(?:\*\*?|__?|~~|`)[{CJK}]'.format(CJK=CJK))
+MD_CJK_TAG_END = re.compile('[{CJK}](?:\*\*?|__?|~~|`)$'.format(CJK=CJK))
+
+CJK_Q_STARTS = r'[「『【〖〔［｛（]'
+CJK_Q_ENDS = r'[」』】〗〕］｝）]'
+
+CJK_Q_STARTS_MATCH = re.compile(
+    '^{CJK_Q_STARTS}'.format(CJK_Q_STARTS=CJK_Q_STARTS))
+CJK_Q_ENDS_MATCH = re.compile('{CJK_Q_ENDS}$'.format(CJK_Q_ENDS=CJK_Q_ENDS))
 
 
 def convert_inline_latex_to_block_latex(text):
-    if not re.fullmatch(r' *\n*\$.*\$ *\n*', text):
+    if not re.fullmatch(r'[ \n]*\$.*\$[ \n]*', text):
         # Is not a latex syntax
         return text
 
-    text = re.sub(r'^ *\n*\$ *\n*', '$$\n', text)
-    text = re.sub(r' *\n*\$ *\n*$', '\n$$\n\n', text)
+    text = re.sub(r'^[ \n]*\$[ \n]*', '\n$$\n', text)
+    text = re.sub(r'[ \n]*\$[ \n]*$', '\n$$\n\n', text)
 
     return text
+
+
+def abstract_inline_conversion_with_spacing(markup_fn):
+    def implementation(self, el, text, convert_as_inline):
+        markup = markup_fn(self)
+        prefix, suffix, text = chomp(text)
+        if not text:
+            return ''
+        if re.match(CJK_Q_STARTS_MATCH, text) and not prefix.startswith(' '):
+            prefix = prefix + ' '
+        if re.search(CJK_Q_ENDS_MATCH, text) and not suffix.endswith(' '):
+            suffix = suffix + ' '
+        return '%s%s%s%s%s' % (prefix, markup, text, markup, suffix)
+    return implementation
 
 
 class MdConverter(MarkdownConverter):
     def process_text(self, el):
         text = super().process_text(el)
+        prefix = ' ' if text and text[0] == ' ' else ''
+        suffix = ' ' if text and text[-1] == ' ' else ''
         text = pangu.spacing_text(text)
-        return text
+        return prefix + text + suffix
+
+    def convert_hn(self, n, el, text, convert_as_inline):
+        text = super().convert_hn(n, el, text, convert_as_inline)
+        # Ensure that there is a blank line before the heading. Additional blank lines will be removed later in `convert_html_to_markdown`.
+        return "\n\n" + text
 
     def convert_math(self, el, text, convert_as_inline):
         return f" ${el['alttext']}$ "
@@ -40,6 +78,96 @@ class MdConverter(MarkdownConverter):
         # LaTeX syntax placed in <p> will become block LaTeX in Wikipedia.
         text = convert_inline_latex_to_block_latex(text)
         return super().convert_p(el, text, convert_as_inline)
+
+    convert_b = abstract_inline_conversion_with_spacing(lambda _: '**')
+    convert_strong = convert_b
+
+    convert_em = abstract_inline_conversion_with_spacing(lambda _: '_')
+    convert_i = convert_em
+
+    convert_del = abstract_inline_conversion_with_spacing(lambda _: '~~')
+    convert_i = convert_em
+
+    def process_tag(self, node, convert_as_inline, children_only=False):
+        """
+        Override `for el in node.children` part to handle CJK spacing.
+        """
+
+        text = ''
+
+        # markdown headings or cells can't include
+        # block elements (elements w/newlines)
+        isHeading = html_heading_re.match(node.name) is not None
+        isCell = node.name in ['td', 'th']
+        convert_children_as_inline = convert_as_inline
+
+        if not children_only and (isHeading or isCell):
+            convert_children_as_inline = True
+
+        # Remove whitespace-only textnodes in purely nested nodes
+        def is_nested_node(el):
+            return el and el.name in ['ol', 'ul', 'li',
+                                      'table', 'thead', 'tbody', 'tfoot',
+                                      'tr', 'td', 'th']
+
+        if is_nested_node(node):
+            for el in node.children:
+                # Only extract (remove) whitespace-only text node if any of the
+                # conditions is true:
+                # - el is the first element in its parent
+                # - el is the last element in its parent
+                # - el is adjacent to an nested node
+                can_extract = (not el.previous_sibling
+                               or not el.next_sibling
+                               or is_nested_node(el.previous_sibling)
+                               or is_nested_node(el.next_sibling))
+                if (isinstance(el, NavigableString)
+                        and six.text_type(el).strip() == ''
+                        and can_extract):
+                    el.extract()
+
+        # Convert the children first
+        for el in node.children:
+            if isinstance(el, Comment) or isinstance(el, Doctype):
+                continue
+            elif isinstance(el, NavigableString):
+                text = concat_text_nodes(
+                    text,
+                    self.process_text(el)
+                )
+            else:
+                text = concat_text_nodes(
+                    text,
+                    self.process_tag(el, convert_children_as_inline)
+                )
+
+        if not children_only:
+            convert_fn = getattr(self, 'convert_%s' % node.name, None)
+            if convert_fn and self.should_convert_tag(node.name):
+                text = convert_fn(node, text, convert_as_inline)
+
+        return text
+
+
+def concat_text_nodes(text, text_2):
+    connector = ''
+    # "你好 **hello**"
+    if re.search(ENDS_WITH_CJK, text) and re.match(MD_ANS_TAG_START, text_2):
+        connector = ' '
+    # "**hello** 你好"
+    elif re.search(MD_ANS_TAG_END, text) and re.match(STARTS_WITH_CJK, text_2):
+        connector = ' '
+    # "**你好** hello"
+    elif re.search(MD_CJK_TAG_END, text) and re.match(STARTS_WITH_ANS, text_2):
+        connector = ' '
+    # "hello **你好**"
+    elif re.search(ENDS_WITH_ANS, text) and re.match(MD_CJK_TAG_START, text_2):
+        connector = ' '
+    if text.endswith(' '):
+        connector = ' '
+    if text_2.startswith(' '):
+        connector = ' '
+    return text.rstrip(' ') + connector + text_2.lstrip(' ')
 
 
 def remove_additional_newlines(s):
@@ -56,10 +184,5 @@ def convert_html_to_markdown(html_text):
 
     text = MdConverter(heading_style=ATX, autolinks=False).convert(html_text)
     text = remove_additional_newlines(text)
-    text = CJK_QUOTE.sub(r'\1 \2', text)
-    text = QUOTE_CJK.sub(r'\1 \2', text)
-    text = CJK_ASTERISK_EN.sub(r'\1 \2\3', text)
-    text = EN_ASTERISK_CJK.sub(r'\1\2 \3', text)
-    text = CJK_UNDERLINE_EN.sub(r'\1 \2\3', text)
-    text = EN_UNDERLINE_CJK.sub(r'\1\2 \3', text)
+
     return text.strip()
---
+++ tools/packages/html_to_markdown_convertor/test.py
@@ -0,0 +1,157 @@
+import unittest
+from html_to_markdown_convertor import convert_html_to_markdown
+import textwrap
+
+
+class TestHTMLToMarkdownConvert(unittest.TestCase):
+    def test_basic_conversion(self):
+        self.assertEqual(
+            convert_html_to_markdown("你好，<b>世界</b>！"),
+            "你好，**世界**！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("你好，<strong>世界</strong>！"),
+            "你好，**世界**！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("你好，<i>世界</i>！"),
+            "你好，_世界_！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("你好，<em>世界</em>！"),
+            "你好，_世界_！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("你好，<s>世界</s>！"),
+            "你好，~~世界~~！"
+        )
+
+        self.assertEqual(
+            convert_html_to_markdown("你好，<del>世界</del>！"),
+            "你好，~~世界~~！"
+        )
+
+        self.assertEqual(
+            convert_html_to_markdown(
+                "<h1>標題</h1><h2>標題</h2>你好。\n\n\n\n<h2>標題</h2>"),
+            dedent("""
+            # 標題
+
+            ## 標題
+
+            你好。
+
+            ## 標題
+            """)
+        )
+
+    def test_basic_spacing(self):
+        self.assertEqual(
+            convert_html_to_markdown("你好，<del>世界</del>！"),
+            "你好，~~世界~~！"
+        )
+
+    def test_more_spacing(self):
+        """
+        These test cases are mainly handled by the `concat_text_nodes` function.
+        """
+
+        self.assertEqual(
+            convert_html_to_markdown("當你凝視著<b>bug</b>，<b>bug</b>也凝視著你"),
+            "當你凝視著 **bug**，**bug** 也凝視著你"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("因為<b>「沒有人」</b>是萬能的！"),
+            "因為 **「沒有人」** 是萬能的！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("因為<b>「沒有人」</b>是萬能的！"),
+            "因為 **「沒有人」** 是萬能的！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Because<b>「nobody」</b>is perfect!"),
+            "Because **「nobody」** is perfect!"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Because no<b>body</b> is perfect!"),
+            "Because no**body** is perfect!"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Because <b>no</b>body is perfect!"),
+            "Because **no**body is perfect!"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("因為<b>沒有人</b>是萬能的！"),
+            "因為**沒有人**是萬能的！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Because<b> nobody </b> is perfect!"),
+            "Because **nobody** is perfect!"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("因為<b> 沒有人 </b>是萬能的！"),
+            "因為 **沒有人** 是萬能的！"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("沒有人是萬能的<del>（茶</del>"),
+            "沒有人是萬能的 ~~（茶~~"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("A<b>「」</b>BCD"),
+            "A **「」** BCD"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("ㄅㄆ<b>「」</b>ㄇㄈ"),
+            "ㄅㄆ **「」** ㄇㄈ"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Hello<i>你好</i>nice to meet you."),
+            "Hello _你好_ nice to meet you."
+        )
+        self.assertEqual(
+            convert_html_to_markdown("將<code>x</code>與<code>y</code>相加"),
+            "將 `x` 與 `y` 相加"
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Press the<code>送出</code>button."),
+            "Press the `送出` button."
+        )
+        self.assertEqual(
+            convert_html_to_markdown("Press the<kbd>送出</kbd>button."),
+            "Press the `送出` button."
+        )
+
+    def test_convert_latex_in_wikipedia(self):
+        html = """
+        <p>兩函數之和的傅立葉轉換等於各自轉換之和。嚴格數學描述是：若函數<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle f\\left(x\\right)}">..</math></span></span>和<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle g\\left(x\\right)}">...</math></span></span>的傅立葉轉換<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle {\\mathcal {F}}[f]}">...</math></span></span>和<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle {\\mathcal {F}}[g]}">...</math></span></span>都存在，<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle \\alpha }"></math></span></span>和<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle \\beta }">...</math></span></span>為任意常係數，則<span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle {\\mathcal {F}}[\\alpha f+\\beta g]=\\alpha {\\mathcal {F}}[f]+\\beta {\\mathcal {F}}[g]}">...</math></span></span>。</p>
+        """
+        self.assertEqual(
+            convert_html_to_markdown(html),
+            "兩函數之和的傅立葉轉換等於各自轉換之和。嚴格數學描述是：若函數 ${\\displaystyle f\\left(x\\right)}$ 和 ${\\displaystyle g\\left(x\\right)}$ 的傅立葉轉換 ${\\displaystyle {\\mathcal {F}}[f]}$ 和 ${\\displaystyle {\\mathcal {F}}[g]}$ 都存在， ${\\displaystyle \\alpha }$ 和 ${\\displaystyle \\beta }$ 為任意常係數，則 ${\\displaystyle {\\mathcal {F}}[\\alpha f+\\beta g]=\\alpha {\\mathcal {F}}[f]+\\beta {\\mathcal {F}}[g]}$ 。"
+        )
+        html = """
+        定義為：<dl><dd><span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle {\\hat {f}}(\\omega )={\\frac {1}{(2\\pi )^{n/2}}}\\int _{\\mathbf {R} ^{n}}f(x)e^{-i\\omega \\cdot x}\\,dx}">...</math></span></span></dd><dd><span class="mwe-math-element"><span class="mwe-math-mathml-inline mwe-math-mathml-a11y" style=""><math xmlns="..." alttext="{\\displaystyle f(x)={\\frac {1}{(2\\pi )^{n/2}}}\\int _{\\mathbf {R} ^{n}}{\\hat {f}}(\\omega )e^{i\\omega \\cdot x}\\,d\\omega .}">...</math></span></span></dd></dl>根據這一形式⋯⋯
+        """
+        self.assertEqual(
+            convert_html_to_markdown(html),
+            dedent("""
+            定義為：
+            $$
+            {\\displaystyle {\\hat {f}}(\\omega )={\\frac {1}{(2\\pi )^{n/2}}}\\int _{\\mathbf {R} ^{n}}f(x)e^{-i\\omega \\cdot x}\\,dx}
+            $$
+
+            $$
+            {\\displaystyle f(x)={\\frac {1}{(2\\pi )^{n/2}}}\\int _{\\mathbf {R} ^{n}}{\\hat {f}}(\\omega )e^{i\\omega \\cdot x}\\,d\\omega .}
+            $$
+
+            根據這一形式⋯⋯
+            """)
+        )
+
+
+def dedent(str):
+    return textwrap.dedent(str).strip()
+
+
+if __name__ == '__main__':
+    unittest.main()
